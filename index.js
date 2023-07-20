6db07b06027f94d262508b3bb11f96895c26c0a0
PradeepaBharathi <bharathi83201@gmail.com>
2023-07-20 20:19:23 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import dotenv from "dotenv";
 import { config } from 'dotenv';
+import cors from "cors";
 // const dotenv = require('dotenv');
 
 import { moneyRouter } from "./routes/incomeRoute.js";
@@ -13,6 +14,7 @@ const app = express();
 
 
 app.use(express.json())
+app.use(cors())
 app.use("/moneytracker",moneyRouter)
 app.use("/expensetracker",expenseRouter)
 
